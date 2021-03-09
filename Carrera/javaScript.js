99960568a4b631f3e0f803d774e5596dffff01ca
DanielAldanaBejarano <daniel.aldanabe@gmail.com>
2021-03-08 20:54:29 -0500

    nada

---
+++ Carrera/javaScript.js
@@ -0,0 +1,16 @@
+
+function running () {
+    if (timeOne)
+    let timeOne = document.getElementById('personOne-time').value 
+    parseInt (timeOne, 10)
+    let timeTwo = document.getElementById('personTwo-time').value   
+    parseInt (timeTwo, 10)   
+    if (timeOne > timeTwo) {
+        alert("sdfasf")
+    }
+}
+
+
+
+
+
